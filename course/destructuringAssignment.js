6277c289a45e796b5624c25c3c471a1e190ffc10
Cheer <luap580101@gmail.com>
2022-05-20 14:34:14 +0800

    解構賦值 Destructuring Assignment

--- course/destructuringAssignment.js
+++ course/destructuringAssignment.js
@@ -2,4 +2,21 @@
 let animals = ["鼠", "牛", "虎", "兔", "龍", "蛇"];
 let [mouse, , tiger, rabbit] = animals;
 
-console.log(tiger);
+// console.log(tiger);
+
+// 物件
+let countries = {
+  taiwan: "台灣",
+  japan: "日本",
+  china: "中國",
+  corea: "韓國", //韓國故意拼錯 korea
+};
+// let { japan, china, taiwan, corea } = countries;
+// console.log(taiwan);
+// console.log(japan);
+// console.log(china);
+// console.log(corea);
+
+let { japan, china, taiwan, corea: korea } = countries;
+// console.log(corea); // undefinded
+console.log(korea);
