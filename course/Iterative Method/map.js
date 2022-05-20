00922cbf67ab5f94f141fbbc45274df20cd9bd81
Cheer <luap580101@gmail.com>
2022-05-20 15:01:13 +0800

    map

---
+++ course/Iterative Method/map.js
@@ -0,0 +1,12 @@
+let animals = [
+  { id: "a1", en: "cat", cn: "貓", num: 20 },
+  { id: "a2", en: "dog", cn: "狗", num: 10 },
+  { id: "a3", en: "mouse", cn: "鼠", num: 50 },
+  { id: "a4", en: "hotDog", cn: "熱狗", num: 2 },
+];
+
+let animalCnName = animals.map((animal, index) => {
+  return animal.cn;
+});
+
+console.log(animalCnName);
