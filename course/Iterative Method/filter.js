b032c29e744107344b30975f57bb78fded108715
Cheer <luap580101@gmail.com>
2022-05-20 15:09:32 +0800

    filter

--- course/Iterative Method/filter.js
+++ course/Iterative Method/filter.js
@@ -1,13 +1,19 @@
 // 過濾
 
 let animals = [
-    { id: "a1", en: "cat", cn: "貓", num: 20 },
-    { id: "a2", en: "dog", cn: "狗", num: 10 },
-    { id: "a3", en: "mouse", cn: "鼠", num: 50 },
-    { id: "a4", en: "hotDog", cn: "熱狗", num: 2 },
-  ];
+  { id: "a1", en: "cat", cn: "貓", num: 20 },
+  { id: "a2", en: "dog", cn: "狗", num: 10 },
+  { id: "a3", en: "mouse", cn: "鼠", num: 50 },
+  { id: "a4", en: "hotDog", cn: "熱狗", num: 2 },
+];
 
-let animalFilter = animals.filter((value, index)=>{
-    return value.num > 15
-})
-console.log(animalFilter)
+let animalFilter = animals.filter((value, index) => {
+  return value.num > 15;
+});
+// console.log(animalFilter)
+
+let animalDel = animals.filter((value, index) => {
+  return index !== 0;
+  // 不要第一筆了, 間接刪掉第一筆
+});
+// console.log(animalDel);
